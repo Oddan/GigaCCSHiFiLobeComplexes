e2aac661176d9fa0e0209518abe3bb42165a9a5c
Odd Andersen <odd.andersen@sintef.no>
2026-01-27 17:44:15 +0100

    Script to test a simple VE simulation on a cross section

--- simple_VE.m
+++ simple_VE.m
@@ -1,10 +1,18 @@
 mrstModule add co2lab
+gravity on
 
 % load Grid
 load('grids')
 
+% convert units
+rocks_mid.perm = rocks_mid.perm * milli * darcy; % from mD to Darcy
+rocks_mid.permz = rocks_mid.permz * milli * darcy; % from mD to Darcy
+rocks_mid.poro = rocks_mid.poro; % porosity is unitless
+
+
 % Define a simple vertical equilibrium grid
 [Gt, G, transMult, discarded_cells] = topSurfaceGrid(Gmid);
+
 
 % loop over fields in rocks_mid and remove values corresponding to discarded
 % cells
@@ -15,6 +23,9 @@ for k = 1:numel(rock_fields)
 end
 
 rockVE = averageRock(rocks_mid, Gt)
+
+% handle vanishing permeabilities at either endpoint
+rockVE.perm(rockVE.perm(:,1) < 1e-15) = min(rockVE.perm(21:end-1));
 
 %% run 3D simulation
 
@@ -42,7 +53,7 @@ muco2   = co2.mu(p_ref, t_ref) * Pascal * second; % co2 viscosity
 invPc3D = @(pc) (1-srw) .* (pe./max(pc, pe)).^2 + srw;
 kr3D    = @(s) max((s-src)./(1-src), 0).^2; % uses CO2 saturation
 fluid   = makeVEFluid(Gt, rockVE, ...
-               'sharp_interface_integrated'           , ...
+               'sharp_interface_simple'           , ...
                'co2_mu_ref'  , muco2                  , ...
                'wat_mu_ref'  , muw                    , ...
                'co2_rho_ref' , co2_rho                , ...
@@ -85,13 +96,20 @@ schedule.control(2) = struct('W', W, 'bc', bc);
 schedule.control(2).W.val = 0;
 
 % Specifying length of simulation timesteps
-schedule.step.val = [repmat(year,    10, 1); ...
-                     repmat(10*year, 10, 1)];
+% $$$ schedule.step.val = [rampupTimesteps(2*year, year/24, 7); ...
+% $$$                      repmat(1 * year, 250, 1)];
+schedule.step.val = [rampupTimesteps(1*year, year/24, 7); ...
+                     rampupTimesteps(100*year, 5 * year, 7)];
+                     
 % Specifying which control to use for each timestep.
 % The first 100 timesteps will use control 1, the last 100
 % timesteps will use control 2.
-schedule.step.control = [ones(10, 1); ...
-                         ones(10, 1) * 2];
+schedule.step.control = [ones(31, 1); ...
+                         ones(27, 1) * 2];
+%schedule.step.control = [ones(55, 1)]
+%ones(57, 1) * 2];
+% $$$ schedule.step.control = [ones(79, 1)];
+                         
 
 % run simulation
 model = CO2VEBlackOilTypeModel(Gt, rockVE, fluid);
@@ -100,3 +118,29 @@ nls = NonLinearSolver('maxTimestepCuts', 12);
 [wellSol, states] = simulateScheduleAD(initState, model, schedule, ...
                                        'NonLinearSolver', nls);
 states = [{initState}; states];
+
+mrstModule add mrst-gui
+plotToolbar(Gt, states);
+
+sat_VE3D = {};
+% converting saturation result to 3D
+for i = 1:numel(states)
+    s = states{i}.s(:,2);
+    smax = states{i}.sGmax;
+    p = states{i}.pressure;
+    [h, h_max] = upscaledSat2height(s, smax, Gt, ...
+                                    'resSat', [srw, src], ...
+                                    'poro', rocks_mid.poro, ...
+                                    'rhoW', fluid.rhoW, ...
+                                    'rhoG', fluid.rhoG, 'p', p);
+    sat_VE3D = [sat_VE3D, {height2finescaleSat(h, h_max, Gt, ...
+                                               fluid.res_water, fluid.res_gas, ...
+                                               'rhoW', fluid.rhoW(p), ...
+                                               'rhoG', fluid.rhoG(p))}];
+end
+% $$$ figure;
+% $$$ plotCellData(G, rocks_mid.poro, 'EdgeColor', 'none');
+% $$$ figure;
+% $$$ plotCellData(G, rocks_mid.perm, 'EdgeColor', 'none');
+figure;
+plotToolbar(G, sat_VE3D);
